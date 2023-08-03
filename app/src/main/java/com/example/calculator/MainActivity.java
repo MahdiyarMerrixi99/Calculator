551acdef69dedba4fe9f99f23659a69cb62a9021
Saba ICT Co <mahdiyarmerrikhi2008@gmail.com>
2023-08-03 16:29:18 +0330

    Calculator

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -16,6 +16,7 @@ import org.mozilla.javascript.Scriptable;
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
 
+
     String data;
 
     @Override
@@ -53,6 +54,11 @@ public class MainActivity extends AppCompatActivity {
             data = binding.inputText.getText().toString();
             binding.inputText.setText(data + "5");
         });
+        binding.exit.setOnClickListener(view -> {
+            Toast.makeText(this, "I said ,Don't click :)", Toast.LENGTH_SHORT).show();
+            finish();
+            System.exit(0);
+        });
 
         binding.button6.setOnClickListener(view -> {
             data = binding.inputText.getText().toString();
@@ -76,7 +82,12 @@ public class MainActivity extends AppCompatActivity {
 
         binding.buttonDot.setOnClickListener(view -> {
             data = binding.inputText.getText().toString();
-            binding.inputText.setText(data + ".");
+            String lastChar = String.valueOf(data.charAt(data.length() - 1));
+            if (lastChar.equals(".")) {
+                binding.inputText.setText(data + "");
+            } else {
+                binding.inputText.setText(data + ".");
+            }
         });
 
         binding.buttonC.setOnClickListener(view -> {
@@ -86,28 +97,53 @@ public class MainActivity extends AppCompatActivity {
 
         binding.buttonPlus.setOnClickListener(view -> {
             data = binding.inputText.getText().toString();
-            binding.inputText.setText(data + "+");
+            String lastChar = String.valueOf(data.charAt(data.length() - 1));
+            if (lastChar.equals("+")) {
+                binding.inputText.setText(data + "");
+            } else {
+                binding.inputText.setText(data + "+");
+            }
         });
 
         binding.buttonMinus.setOnClickListener(view -> {
             data = binding.inputText.getText().toString();
-            binding.inputText.setText(data + "-");
+            String lastChar = String.valueOf(data.charAt(data.length() - 1));
+            if (lastChar.equals("-")) {
+                binding.inputText.setText(data + "");
+            } else {
+                binding.inputText.setText(data + "-");
+            }
         });
 
         binding.buttonMultiplication.setOnClickListener(view -> {
             data = binding.inputText.getText().toString();
-            binding.inputText.setText(data + "×");
+            String lastChar = String.valueOf(data.charAt(data.length() - 1));
+            if (lastChar.equals("×")) {
+                binding.inputText.setText(data + "");
+            } else {
+                binding.inputText.setText(data + "×");
+            }
         });
         binding.buttonDistribution.setOnClickListener(view -> {
             data = binding.inputText.getText().toString();
-            binding.inputText.setText(data + "÷");
+            String lastChar = String.valueOf(data.charAt(data.length() - 1));
+            if (lastChar.equals("÷")) {
+                binding.inputText.setText(data + "");
+            } else {
+                binding.inputText.setText(data + "÷");
+            }
         });
 
         binding.buttonModule.setOnClickListener(view -> {
             data = binding.inputText.getText().toString();
-            binding.inputText.setText(data + "%");
+            String lastChar = String.valueOf(data.charAt(data.length() - 1));
+            if (lastChar.equals("%")) {
+                binding.inputText.setText(data + "");
+            } else {
+                binding.inputText.setText(data + "%");
+            }
         });
-        
+
         binding.fun.setOnClickListener(view -> {
             Toast.makeText(this, "This button has a fan aspect", Toast.LENGTH_SHORT).show();
         });
@@ -115,29 +151,36 @@ public class MainActivity extends AppCompatActivity {
         binding.buttonEquls.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                data=binding.inputText.getText().toString();
-                data=data.replaceAll("×","*");
-                data=data.replaceAll("/","/100");
-                data=data.replaceAll("÷","/");
+                data = binding.inputText.getText().toString();
+                data = data.replaceAll("×", "*");
+                data = data.replaceAll("%", "/100");
+                data = data.replaceAll("÷", "/");
+                String lastChar = String.valueOf(data.charAt(data.length() - 1));
+                switch (lastChar) {
+                    case "+":
+                        Toast.makeText(MainActivity.this, R.string.error_text, Toast.LENGTH_SHORT).show();
+                        break;
+                    case "-":
+                        Toast.makeText(MainActivity.this, R.string.error_text, Toast.LENGTH_SHORT).show();
+                        break;
+                    case "%":
+                        Toast.makeText(MainActivity.this, R.string.error_text, Toast.LENGTH_SHORT).show();
+                        break;
+                    case "÷":
+                        Toast.makeText(MainActivity.this, R.string.error_text, Toast.LENGTH_SHORT).show();
+                        break;
+                    case "×":
+                        Toast.makeText(MainActivity.this, R.string.error_text, Toast.LENGTH_SHORT).show();
 
-                Context rhino =Context.enter();
+                }
+                Context rhino = Context.enter();
                 rhino.setOptimizationLevel(-1);
-
-                String finalResult="";
-
-                Scriptable scriptable=rhino.initStandardObjects();
-                finalResult=rhino.evaluateString(scriptable,data,"Javsscript",1,null).toString();
+                String finalResult = "";
+                Scriptable scriptable = rhino.initStandardObjects();
+                finalResult = rhino.evaluateString(scriptable, data, "Javsscript", 1, null).toString();
 
                 binding.outputText.setText(finalResult);
             }
         });
-
-        binding.exit.setOnClickListener(view -> {
-            Toast.makeText(this, "I said ,Don't click :)", Toast.LENGTH_SHORT).show();
-            finish();
-
-            System.exit(0);
-        });
-
     }
 }
